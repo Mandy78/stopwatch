5b3bd3a3fd53730a1cd52b5a1a35258fe8c06f25
Mandy78 <sodhi_mandeepsingh@yahoo.com>
2022-04-11 21:42:21 +0530

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useState} from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Button from "./Button";
+
+
+
+
+const App  = () => {
+
+    const [timerValue, setTimerValue] = useState(0);
+    const [timerId,  setTimerId] =  useState(0);
+    
+    const startHandler = () => {
+        
+         setInterval( () => { setTimerValue((timerValue)=>timerValue+1);}
+                        ,1000 )
+                  
+                                                   
+    }
+
+    const stopHandler = () => {
+        
+          clearInterval(timerId);
+
+    }
+    
+    const resetHandler = () => {
+      
+        setTimerValue(0);
+        clearInterval(timerId);
+    }
+  
+    return(
+        <div className="container">
+         
+            <div className="wrapper">
+            <p>
+                <span className="hrs">0</span>:
+                <span className="mins">0</span>:
+                <span className="secs">{timerValue}</span>
+            </p> <br></br>
+             <Button click= {startHandler}  title= "Start"/>
+             <Button click= {stopHandler}   title= "Stop"/>
+             <Button click= {resetHandler}  title= "Reset"/> 
+            </div>
     </div>
-  );
-}
+    )
 
+}
 export default App;
---
+++ src/Button.js
@@ -0,0 +1,14 @@
+import "./Button.css";
+
+
+const Button = (props)=>{
+
+     
+    return(
+        <button  className="btn" onClick={props.click}>{props.title}</button>
+    )
+
+
+}
+
+export default Button;
